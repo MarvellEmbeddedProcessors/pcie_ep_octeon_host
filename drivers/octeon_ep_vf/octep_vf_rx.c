6c3b468d619f78e372f23f0f1ec3173d4fab7723
Veerasenareddy Burru <vburru@cavium.com>
2022-12-03 19:52:06 -0800

    octeon_ep_vf: check PCI read failures for tx/rx pkt counts
    Check tx/rx pkt count CSR reads failures that can happen when the device
    suddenly becomes unresponsive due to a crash and reset on device.
    
    Signed-off-by: Veerasenareddy Burru <vburru@cavium.com>
    Change-Id: I7bc7cbff95e4149944cb06251096b5cec401c149

--- drivers/octeon_ep_vf/octep_vf_rx.c
+++ drivers/octeon_ep_vf/octep_vf_rx.c
@@ -321,6 +321,11 @@ static int octep_vf_oq_check_hw_for_pkts(struct octep_vf_device *oct,
 	u32 pkt_count, new_pkts;
 
 	pkt_count = readl(oq->pkts_sent_reg);
+	if (unlikely(pkt_count == 0xFFFFFFFF)) {
+		pkt_count = 0;
+		dev_emerg(oq->dev, "OQ-%u count read failure\n", oq->q_no);
+		return 0;
+	}
 	new_pkts = pkt_count - oq->last_pkt_count;
 
 	/* Clear the hardware packets counter register if the rx queue is
