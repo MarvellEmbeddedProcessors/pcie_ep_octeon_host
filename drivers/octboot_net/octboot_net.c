4144e973ccd9ec82bd0b1f4c566ecce107e5c780
Satananda Burla <sburla@marvell.com>
2022-12-18 17:16:16 -0800

    host/octboot_net: add RHEL build support
    add RHEL build support
    
    Signed-off-by: Satananda Burla <sburla@marvell.com>
    Change-Id: I9421ca7e3184d9a2ccff64cd650338d75c29e645

--- drivers/octboot_net/octboot_net.c
+++ drivers/octboot_net/octboot_net.c
@@ -16,6 +16,17 @@
 #include <linux/sched/signal.h>
 #endif
 
+#if defined(RHEL_RELEASE_CODE)
+#if  (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(8,4))
+#define HAS_2PARAM_ACCESS_OK
+#define NO_HAS_MMIOWB
+#define HAS_MULTI_TXQUEUE
+#define HAS_SELECT_QUEUE_FALLBACK
+#define NO_HAS_XMIT_MORE
+#define HAS_SKB_FRAG_OFF
+#endif
+#endif
+
 #include "desc_queue.h"
 #include "octboot_net.h"
 #include "mmio_api.h"
