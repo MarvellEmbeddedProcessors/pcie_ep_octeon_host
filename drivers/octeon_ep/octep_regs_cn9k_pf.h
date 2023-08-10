7ca4a9768676013f654681dc1617e006e41d40da
Shinas Rasheed <srasheed@marvell.com>
2023-08-10 12:24:56 -0700

    octeon_ep: set firmware ready to 2 on driver load for cn95n chips
    Firmware ready has to be set to 2 on driver load due HW bug not
    clearing fw status ready value at early boot. This feature was
    implemented for cn10k chips. This commit adds the same for cn95n card
    as well
    
    Signed-off-by: Shinas Rasheed <srasheed@marvell.com>
    Change-Id: Icafb63aac7e8d7cf89d504131943b5d4e0a4fae5
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/pcie_ep/pcie_ep_octeon_host/+/109615
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Haseeb Gani <hgani@marvell.com>

--- drivers/octeon_ep/octep_regs_cn9k_pf.h
+++ drivers/octeon_ep/octep_regs_cn9k_pf.h
@@ -382,6 +382,15 @@
 #define CN93_SDP_EPF_OEI_RINT_DATA_BIT_MBOX	BIT_ULL(0)
 /* bit 1 for firmware heartbeat interrupt */
 #define CN93_SDP_EPF_OEI_RINT_DATA_BIT_HBEAT	BIT_ULL(1)
+#define FW_STATUS_RUNNING      2ULL
+#define CN9K_PEMX_PFX_CSX_PFCFGX(pem, pf, offset)      ((0x8e0000008000 | (uint64_t)pem << 36 \
+							| pf << 18 \
+							| ((offset >> 16) & 1) << 16 \
+							| (offset >> 3) << 3) \
+							+ (((offset >> 2) & 1) << 2))
+
+/* Register defines for use with CN9K_PEMX_PFX_CSX_PFCFGX */
+#define CN9K_PCIEEP_VSECST_CTL  0x4D0
 
 #define CN93_PEM_BAR4_INDEX		7
 #define CN93_PEM_BAR4_INDEX_SIZE	0x400000ULL
